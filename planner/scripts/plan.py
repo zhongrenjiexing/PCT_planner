9c87d67629863a45005d78e437bd747233064783
zhongrenjiexing <994585972@qq.com>
2025-12-17 19:49:32 +0800

    update default scene

--- planner/scripts/plan.py
+++ planner/scripts/plan.py
@@ -1,3 +1,4 @@
+# conda env: unitreerl
 import sys
 import argparse
 import numpy as np
@@ -12,7 +13,7 @@ sys.path.append('../')
 from config import Config
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--scene', type=str, default='Spiral', help='Name of the scene. Available: [\'Spiral\', \'Building\', \'Plaza\']')
+parser.add_argument('--scene', type=str, default='sii_l6', help='Name of the scene. Available: [\'Spiral\', \'Building\', \'Plaza\']')
 args = parser.parse_args()
 
 cfg = Config()
@@ -22,7 +23,7 @@ if args.scene == 'Spiral':
     # start_pos = np.array([-16.0, -6.0], dtype=np.float32)
     start_pos = np.array([-16.0, -6.0, 0], dtype=np.float32)
     # end_pos = np.array([-26.0, -5.0], dtype=np.float32)
-    end_pos = np.array([-43.0, -3.8, 16.5], dtype=np.float32) # 0.5 / 8.5 / 16.5
+    end_pos = np.array([-43.0, -3.8, 8.5], dtype=np.float32) # 0.5 / 8.5 / 16.5
 elif args.scene == 'Building':
     tomo_file = 'building2_9'
     start_pos = np.array([5.0, 5.0], dtype=np.float32)
